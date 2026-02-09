bd02c9ff097221c1347b25e2a0255d46cc2cea67
fuxkcom <ediffiorlife@gmail.com>
2026-02-09 09:22:28 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect, useMemo, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { supabase, Post, Profile, Comment, Mood } from '../lib/supabase'
